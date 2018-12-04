6325ed8b8ba8a7bceb8d093db09bc90c556ec8db
Demetri Lee <git@github.com:demetri77/springboard-projects.git>
2018-12-04 01:26:57 -0500

    Rmd udpates

--- capstone_wrangling.R
+++ capstone_wrangling.R
@@ -6,7 +6,8 @@ library(tidyverse)
 bankfull <- read_delim("bank-additional-full.csv", delim = ";", col_types = "iccccccccciiiicdddddc")
 
 #str(bankfull)
-glimpse(bankfull)
+bankfull <- rename(bankfull, edu_lvl = "education")
+bankfull <- rename(bankfull, contact_typ = "contact")
 bankfull <- rename(bankfull, contact_cnt = "campaign")
 bankfull <- rename(bankfull, days_passed = "pdays")
 bankfull <- rename(bankfull, prev_contact_cnt = "previous")
@@ -16,6 +17,12 @@ bankfull <- rename(bankfull, cons_price_idx = "cons.price.idx")
 bankfull <- rename(bankfull, cons_conf_idx = "cons.conf.idx")
 bankfull <- rename(bankfull, num_employed = "nr.employed")
 bankfull <- rename(bankfull, subscribed = y)
+
+
+etemp <- bankfull$edu_lvl
+bankfull$edu_lvl <- gsub("\\.", "_", etemp)
+
+glimpse(bankfull)
 
 summary(bankfull)
 colnames(bankfull)
@@ -27,12 +34,12 @@ ggplot(bankfull, aes(x = pre_outcome)) + geom_bar()
 
 
 bankfull %>% 
-  select(education, job) %>% 
-  group_by(education, job) %>% 
-  arrange(desc(education)) %>% 
+  select(edu_lvl, job) %>% 
+  group_by(edu_lvl, job) %>% 
+  arrange(desc(edu_lvl)) %>% 
   slice(1:3)
 
-bankfull %>% group_by(education) %>% summarise(count = n()) %>% arrange(desc(count))
+bankfull %>% group_by(edu_lvl) %>% summarise(count = n()) %>% arrange(desc(count))
 bankfull %>% group_by(job) %>% summarise(count = n()) %>% arrange(desc(count))
 
 ggplot(bankfull, aes(x = euribor3m, y = cons.price.idx)) + geom_point()
@@ -41,8 +48,14 @@ ggplot(bankfull, aes(y = euribor3m, x = subscribed)) + geom_point()
 ## filter, select, arrange, mutate, summarise, group_by
 
 ## Variable selection
+ggplot(bankfull, aes(x = job)) + geom_histogram(stat="count")
+ggplot(bankfull, aes(x = job, fill=marital)) + geom_histogram(stat="count")
+ggplot(bankfull, aes(x = job, fill=edu_lvl)) + geom_histogram(stat="count")
 
-#ggplot(bankfull, aes(x = job, fill=marital)) + geom_histogram(stat="count")
+ggplot(bankfull, aes(x = edu_lvl, fill=subscribed)) + geom_histogram(stat="count")
+ggplot(bankfull, aes(x = job, fill=subscribed)) + geom_histogram(stat="count")
+
+ggplot(bankfull, aes(x=euribor3m, fill=subscribed)) + geom_bar(stat="count")
 
 # Outcome of previous campaign
 ggplot(bankfull, aes(x = poutcome)) + geom_bar()
